68e1ed1ebd4c271a6fab973e23c5fbea75a44029
Ferlern <67915420+Ferlern@users.noreply.github.com>
2022-10-08 21:03:38 +0300

    player cursor tracker refactoring

--- scripts/ui/units/player-tracker.js
+++ scripts/ui/units/player-tracker.js
@@ -7,67 +7,32 @@ exports.drawCursor = function(player) {
     const cursorX = player.mouseX;
     const cursorY = player.mouseY;
     const style = Core.settings.getInt("eui-playerCursorStyle");
+    const drawLine = () => {
+        Lines.stroke(1, player.team().color);
+        Draw.alpha(0.7);
+        Lines.line(unitX, unitY, cursorX, cursorY);
+        Draw.reset(); // it is necessary?
+    }
 
-     if (style == 1) { //square (Inspired from Mindustry Ranked Server's spectator mode )
-         Draw.draw(Layer.overlayUI+0.01, () => {
-             Drawf.square(cursorX, cursorY, 2, player.team().color);
-             Draw.alpha(0.7);
-             Draw.reset();
-         }); return;
-     } else if (style == 2) { //square + line
-         Draw.draw(Layer.overlayUI+0.01, () => {
-             Lines.stroke(1, player.team().color);
-             Draw.alpha(0.7);
-             Lines.line(unitX, unitY, cursorX, cursorY);
-             Draw.reset();
-         });
-         Draw.draw(Layer.overlayUI+0.01, () => {
-             Drawf.square(cursorX, cursorY, 2, player.team().color);
-             Draw.alpha(0.7);
-             Draw.reset();
-         }); return;
-     } else if (style == 3) { //Circle
-         Draw.draw(Layer.overlayUI+0.01, () => {
-             Drawf.circles(cursorX, cursorY, 1, player.team().color);
-             Draw.alpha(0.7);
-             Draw.reset();
-         }); return;
-     } else if (style == 4) { //Circle + line
-         Draw.draw(Layer.overlayUI+0.01, () => {
-             Lines.stroke(1, player.team().color);
-             Draw.alpha(0.7);
-             Lines.line(unitX, unitY, cursorX, cursorY);
-             Draw.reset();
-         });
-         Draw.draw(Layer.overlayUI+0.01, () => {
-             Drawf.circles(cursorX, cursorY, 1, player.team().color);
-             Draw.alpha(0.7);
-             Draw.reset();
-         }); return;
-     } else if (style == 5) { //Target
-         Draw.draw(Layer.overlayUI+0.01, () => {
-             Drawf.target(cursorX, cursorY, 3, player.team().color);
-             Draw.alpha(0.7);
-             Draw.reset();
-         }); return;
-     } else if (style == 6) { // Target + Line
-         Draw.draw(Layer.overlayUI+0.01, () => {
-             Lines.stroke(1, player.team().color);
-             Draw.alpha(0.7);
-             Lines.line(unitX, unitY, cursorX, cursorY);
-             Draw.reset();
-         });
-         Draw.draw(Layer.overlayUI+0.01, () => {
-             Drawf.target(cursorX, cursorY, 3, player.team().color);
-             Draw.alpha(0.7);
-             Draw.reset();
-         }); return;
-     } else { //Line (original)
-         Draw.draw(Layer.overlayUI+0.01, () => {
-             Lines.stroke(1, player.team().color);
-             Draw.alpha(0.7);
-             Lines.line(unitX, unitY, cursorX, cursorY);
-             Draw.reset();
-         });
-     }
+    Draw.draw(Layer.overlayUI+0.01, () => {
+        if (style == 1) { //square (Inspired from Mindustry Ranked Server's spectator mode )
+            Drawf.square(cursorX, cursorY, 2, player.team().color);
+        } else if (style == 2) { //square + line
+            drawLine()
+            Drawf.square(cursorX, cursorY, 2, player.team().color);
+        } else if (style == 3) { //Circle
+            Drawf.circles(cursorX, cursorY, 1, player.team().color);
+        } else if (style == 4) { //Circle + line
+            drawLine()
+            Drawf.circles(cursorX, cursorY, 1, player.team().color);
+        } else if (style == 5) { //Target
+            Drawf.target(cursorX, cursorY, 3, player.team().color);
+        } else if (style == 6) { // Target + Line
+            drawLine()
+            Drawf.target(cursorX, cursorY, 3, player.team().color);
+        } else { //Line (original)
+            drawLine()
+        }
+    });
+    Draw.reset();
 }
